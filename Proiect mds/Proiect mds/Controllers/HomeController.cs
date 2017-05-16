f4b08e90d774177e8cc706d5b146711b8fb89110
vladmatei139 <vladmatei139@gmail.com>
2017-05-17 00:02:38 +0300

    Final form
    Registration, login, logout

---
+++ Proiect mds/Proiect mds/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proiect_mds.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
